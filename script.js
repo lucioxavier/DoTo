cb71dc43956e6aadda5c3a7a2c8bd55c9a818159
lucioxavier <lucioflaviowe@hotmail.com>
2021-02-07 23:27:17 -0300

    Definição de paleta + melhorias no botão principal + criação de estrutura basica dos campos de tarefas

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var texto = ""
-button1 = window.document.getElementById("cx1")
-button1.addEventListener('click', function(e){
-    button1.value = texto
+/* var button1 = window.document.getElementById("cx1")
 
-})
+window.addEventListener('click', function(e){
+    if(document.getElementById('caixa_nova_tarefa').contains(e.target)){
+        if(e.target.value == 'Criar uma tarefa...'){
+            button1.value = ''
+        }else{'não apaga o texto'}
+    }/* else{
+        button1.value = "Criar uma tarefa..."
+    } */
+}) */
